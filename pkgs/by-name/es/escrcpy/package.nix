b18c65d8ab1a53b3664db3cb705e04de9f940474
Yjin <im@yjin.dev>
2026-05-17 21:23:43 -0700

    feat(pkgs): implement escrcpy derivation

--- pkgs/by-name/es/escrcpy/package.nix
+++ pkgs/by-name/es/escrcpy/package.nix
@@ -1,25 +1,146 @@
-{ lib, stdenv, fetchurl, dpkg }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+  dpkg,
+  wrapGAppsHook3,
+  alsa-lib,
+  at-spi2-atk,
+  atk,
+  cairo,
+  cups,
+  dbus,
+  expat,
+  fontconfig,
+  freetype,
+  gdk-pixbuf,
+  glib,
+  gtk3,
+  libX11,
+  libXScrnSaver,
+  libXcomposite,
+  libXcursor,
+  libXdamage,
+  libXext,
+  libXfixes,
+  libXi,
+  libXrandr,
+  libXrender,
+  libXtst,
+  libdrm,
+  libnotify,
+  libsecret,
+  libuuid,
+  libxcb,
+  libxkbcommon,
+  mesa,
+  nspr,
+  nss,
+  pango,
+  systemd,
+}:
+
+let
+  version = "2.10.2";
+  sources = {
+    x86_64-linux = {
+      url = "https://github.com/viarotel-org/escrcpy/releases/download/v${version}/Escrcpy-${version}-linux-amd64.deb";
+      hash = "sha256-K6ieDMumPRaj40D85uPqozBhUyHUGLIYt8H2kF/6Rd0=";
+    };
+    aarch64-linux = {
+      url = "https://github.com/viarotel-org/escrcpy/releases/download/v${version}/Escrcpy-${version}-linux-arm64.deb";
+      hash = "sha256-FTI+6Sz6962VdLXuZxveHX/5ZWv7a0I0d3AnhSt+Qeo=";
+    };
+  };
+  srcConfig =
+    sources.${stdenv.hostPlatform.system}
+      or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+in
 stdenv.mkDerivation {
   pname = "escrcpy";
-  version = "2.10.2";
+  inherit version;
+
   src = fetchurl {
-    url = "https://github.com/viarotel-org/escrcpy/releases/download/v2.10.2/Escrcpy-2.10.2-linux-amd64.deb";
-    hash = "sha256-K6ieDMumPRaj40D85uPqozBhUyHUGLIYt8H2kF/6Rd0=";
+    inherit (srcConfig) url hash;
   };
 
-  nativeBuildInputs = [ dpkg ];
+  nativeBuildInputs = [
+    autoPatchelfHook
+    dpkg
+    wrapGAppsHook3
+  ];
 
-  unpackPhase = "dpkg -x $src .";
+  buildInputs = [
+    alsa-lib
+    at-spi2-atk
+    atk
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk3
+    libX11
+    libXScrnSaver
+    libXcomposite
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXi
+    libXrandr
+    libXrender
+    libXtst
+    libdrm
+    libnotify
+    libsecret
+    libuuid
+    libxcb
+    libxkbcommon
+    mesa
+    nspr
+    nss
+    pango
+    systemd
+  ];
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  unpackPhase = ''
+    runHook preUnpack
+    dpkg-deb -x $src .
+    runHook postUnpack
+  '';
 
   installPhase = ''
-    mkdir -p $out
-    cp -r usr/* $out/
+    runHook preInstall
+
+    mkdir -p $out/bin $out/share/escrcpy
+    cp -av opt/Escrcpy/. $out/share/escrcpy/
+    cp -av usr/share/. $out/share/
+
+    ln -s $out/share/escrcpy/escrcpy $out/bin/escrcpy
+
+    # Fix the desktop file
+    substituteInPlace $out/share/applications/escrcpy.desktop \
+      --replace "/opt/Escrcpy/escrcpy" "escrcpy"
+
+    runHook postInstall
   '';
 
   meta = with lib; {
     description = "A graphical interface for scrcpy built with Electron";
     homepage = "https://github.com/viarotel-org/escrcpy";
     license = licenses.gpl3Only;
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
     mainProgram = "escrcpy";
   };
 }
