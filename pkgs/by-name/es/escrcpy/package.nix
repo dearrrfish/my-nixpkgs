e28cf1d5ad24f8a624df32e5bb788483b9e5dcb5
Yjin <im@yjin.dev>
2026-05-17 21:21:56 -0700

    fix(pkgs): add missing meta and fix build for escrcpy

--- pkgs/by-name/es/escrcpy/package.nix
+++ pkgs/by-name/es/escrcpy/package.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchurl }:
+{ lib, stdenv, fetchurl, dpkg }:
 stdenv.mkDerivation {
   pname = "escrcpy";
   version = "2.10.2";
@@ -6,7 +6,20 @@ stdenv.mkDerivation {
     url = "https://github.com/viarotel-org/escrcpy/releases/download/v2.10.2/Escrcpy-2.10.2-linux-amd64.deb";
     hash = "sha256-K6ieDMumPRaj40D85uPqozBhUyHUGLIYt8H2kF/6Rd0=";
   };
-  meta = {
+
+  nativeBuildInputs = [ dpkg ];
+
+  unpackPhase = "dpkg -x $src .";
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r usr/* $out/
+  '';
+
+  meta = with lib; {
+    description = "A graphical interface for scrcpy built with Electron";
+    homepage = "https://github.com/viarotel-org/escrcpy";
+    license = licenses.gpl3Only;
     mainProgram = "escrcpy";
   };
 }
