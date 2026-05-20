aab2ad1db75455d76b715cea06028b9bed0b0423
Yu Jin <im@yjin.dev>
2026-05-19 20:51:15 -0700

    style: format modules/pkgs.nix

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -6,10 +6,12 @@
     {
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
-        config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-          "android-sdk-platform-tools"
-          "platform-tools"
-        ];
+        config.allowUnfreePredicate =
+          pkg:
+          builtins.elem (lib.getName pkg) [
+            "android-sdk-platform-tools"
+            "platform-tools"
+          ];
         overlays = [
           (_final: _prev: {
             local = config.packages;
