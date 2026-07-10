6b6e48536176f7ef7512c3708477a2dd251c16df
Yu Jin <im@yjin.dev>
2026-07-10 00:58:02 -0700

    feat(pkgs): add doubao-murmur

---
+++ pkgs/by-name/do/doubao-murmur/package.nix
@@ -0,0 +1,72 @@
+{ lib
+, python3Packages
+, fetchFromGitHub
+, gobject-introspection
+, wrapGAppsHook4
+, webkitgtk_6_0
+, wl-clipboard
+, xclip
+, xdotool
+, ydotool
+, wtype
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "doubao-murmur";
+  version = "1.4.5";
+
+  src = fetchFromGitHub {
+    owner = "lilong7676";
+    repo = "doubao-murmur";
+    rev = "v${version}";
+    hash = "sha256-axQblvdvtX9XMLXpz4Kjq8BpNlEcVkOzwbQqfz5egCg=";
+  };
+
+  sourceRoot = "source/linux";
+
+  pyproject = true;
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-warn 'build-backend = "setuptools.backends._legacy:_Backend"' 'build-backend = "setuptools.build_meta"'
+  '';
+
+  build-system = [
+    python3Packages.setuptools
+    python3Packages.wheel
+  ];
+
+  nativeBuildInputs = [
+    gobject-introspection
+    wrapGAppsHook4
+  ];
+
+  buildInputs = [
+    webkitgtk_6_0
+  ];
+
+  propagatedBuildInputs = with python3Packages; [
+    pygobject3
+    websockets
+    sounddevice
+  ];
+
+  nativeCheckInputs = with python3Packages; [
+    pytestCheckHook
+    pytest-asyncio
+  ];
+
+  # Prepend the paths of clipboard and simulation CLI tools to the environment wrapper's PATH
+  makeWrapperArgs = [
+    "--prefix PATH : ${lib.makeBinPath [ wl-clipboard xclip xdotool ydotool wtype ]}"
+  ];
+
+  meta = with lib; {
+    description = "Voice-to-text input using Doubao ASR for SteamOS/Linux";
+    homepage = "https://github.com/lilong7676/doubao-murmur";
+    license = licenses.mit;
+    maintainers = [ ];
+    mainProgram = "doubao-murmur";
+    platforms = platforms.linux;
+  };
+}
